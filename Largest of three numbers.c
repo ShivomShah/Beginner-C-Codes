b8ca7218192b4e11952be32b86a9062297a510ad
ShivomShah <shivomshah2522@gmail.com>
2025-10-12 23:34:31 +0530

    Create Largest of three numbers.c

---
+++ Largest of three numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b && a > c)
+        printf("%d is largest\n", a);
+    else if (b > a && b > c)
+        printf("%d is largest\n", b);
+    else if (c > a && c > b)
+        printf("%d is largest\n", c);
+    else
+        printf("Some numbers are equal\n");
+    return 0;
+}
