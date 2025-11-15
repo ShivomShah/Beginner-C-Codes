bc4dfa67f64bbeb17c56c50d530b1ceaac00d687
ShivomShah <shivomshah2522@gmail.com>
2025-11-15 22:21:15 +0530

    Add program to calculate sum of squares of first n natural numbers

---
+++ Sum of square of first n natural Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+
+    printf("Enter value of N: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        sum = sum + (i * i);
+    }
+
+    printf("Sum of squares = %d", sum);
+
+    return 0;
+}
